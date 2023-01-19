83054718c414ddc0124954ddb55cd836ba0d5575
Jun <wnsgur4092@gmail.com>
2023-01-19 16:04:32 +1100

    Initial Commit

---
+++ CurrencyCalculator/CurrencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCalculatorApp.swift
+//  CurrencyCalculator
+//
+//  Created by JunHyuk Lim on 19/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
